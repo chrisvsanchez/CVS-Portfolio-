b2839bbb9d6c0ee7b3a4b17b2410e755a7827e7f
chrisvsanchez <chrisvsanchez@gmail.com>
2020-12-22 00:53:32 -0500

    added react-text-loop library for landing page

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Container, Row, Col } from "react-grid-csystem";
+import TextLoop from "react-text-loop";
 import {
   FaGithub,
   FaMedium,
@@ -8,43 +8,80 @@ import {
   FaEnvelope,
 } from "react-icons/fa";
 import { Link } from "gatsby";
+import styled from "styled-components";
+const OutterDiv = styled.div`
+  height: 80vh;
+  width: 93vw;
+  border: 1px solid red;
+  text-align: center;
+`;
+const CustomBox = styled.div`
+  border: 1px solid blue;
+  height: 80vh; /* Magic here */
+  width: 93vw;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 
+  margin: auto h3, h1 {
+    color: rgb(109, 182, 91);
+  }
+`;
+const StyledLink = styled(Link)`
+  color: purple;
+`;
 const LandingPage = () => {
   return (
-    <div className="landing-page" style={{ height: "90vh", width: "90vw" }}>
-      <div>
-        <Container>
-          <h1>Chris V. Sanchez </h1>
-          <Row className="svg-container">
-            <Col sm={1}>
-              <Link to="https://github.com/chrisvsanchez">
-                <FaGithub />
-              </Link>
-            </Col>
-            <Col sm={1}>
-              <Link to="https://medium.com/@chrisvsanchez">
-                <FaMedium />
-              </Link>
-            </Col>
-            <Col sm={1}>
-              <Link to="https://www.linkedin.com/in/chrisvsanchez/">
-                <FaLinkedin />
-              </Link>
-            </Col>
-            <Col sm={1}>
-              <Link to="https://twitter.com/chris_vsanchez">
-                <FaTwitter />
-              </Link>
-            </Col>
-            <Col sm={1}>
-              <Link to="chrisvsanchez@gmail.com">
-                <FaEnvelope />
-              </Link>
-            </Col>
-          </Row>
-        </Container>
-      </div>
-    </div>
+    <OutterDiv>
+      <CustomBox>
+        {/* <h1>Chris V. Sanchez </h1> */}
+        <div>
+          {" "}
+          <h1 className="landing-page-name">Chris V. Sanchez </h1>
+          <h2>
+            <TextLoop>
+              <span>Full Stack Developer </span>
+              <span>Problem Solving </span>
+            </TextLoop>
+            All day
+          </h2>
+          <br></br>
+          <div></div>
+          <table style={{ overflowX: "auto", border: "0" }}>
+            <tr>
+              <th>
+                <Link to="https://github.com/chrisvsanchez">
+                  <FaGithub />
+                </Link>
+              </th>
+              <td>
+                <Link to="https://medium.com/@chrisvsanchez">
+                  <FaMedium />
+                </Link>
+              </td>
+              <td>
+                <Link to="https://www.linkedin.com/in/chrisvsanchez/">
+                  <FaLinkedin />
+                </Link>
+              </td>
+              <td>
+                <Link to="https://twitter.com/chris_vsanchez">
+                  <FaTwitter />
+                </Link>
+              </td>
+              <td>
+                <Link
+                  to="chrisvsanchez@gmail.com"
+                  style={{ textDecoration: "none" }}
+                >
+                  <FaEnvelope />
+                </Link>
+              </td>
+            </tr>
+          </table>
+        </div>
+      </CustomBox>
+    </OutterDiv>
   );
 };
 export default LandingPage;
